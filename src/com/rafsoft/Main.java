a57d9e05ec24ae325882e6ebfe0493f8422bbfd0
rwedzony <rwedzony@gmail.com>
2021-03-05 13:35:24 +0100

    inital commit

---
+++ src/com/rafsoft/Main.java
@@ -0,0 +1,15 @@
+package com.rafsoft;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable= 3;
+        Logger logger = Logger.getAnonymousLogger();
+
+
+
+        System.out.println("Hello world");
+    }
+}
