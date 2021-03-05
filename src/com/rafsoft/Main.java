5b8fe542c654d2da66b1f6aa8214eb88a8a9fb04
rwedzony <rwedzony@gmail.com>
2021-03-05 13:36:22 +0100

    added anonymous laogger

--- src/com/rafsoft/Main.java
+++ src/com/rafsoft/Main.java
@@ -5,11 +5,7 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
-        int myVariable= 3;
         Logger logger = Logger.getAnonymousLogger();
-
-
-
-        System.out.println("Hello world");
+        logger.info("Inital commit");
     }
 }
